26ad1b3cd45acfe13997fbc69e5f86feca78bea8
Cooper-32 <marcelwomofsky@gmail.com>
2026-02-08 20:14:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function addDistance() {
 }
 
 function loadProgressBar() {
-progress = localStorage.getItem("progress");
+progress = localStorage.getItem("progress") || 0;
 barItem.style.width = progress + "%";
 displayResult.innerHTML="BITCH";
 }
